f576bbcd99dc9d3a57b709bbe56a8cb2214bf0f1
sssssqew <sunrise.in.my.dream@gmail.com>
2024-05-24 18:25:10 +0900

    esbuild + react 빌드 튜토리얼

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+import logo from './logo.svg'
+
+function App(){
+    return (
+        <div className='App'>
+            <h1>Hello, ebsuild ! </h1>
+            <img src={logo} alt="logo" width={500} height={500}/>
+        </div>
+    )
+}
+export default App
