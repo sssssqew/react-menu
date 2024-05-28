211b16871dd728383469508408780a395f6f80f7
sssssqew <sunrise.in.my.dream@gmail.com>
2024-05-28 12:46:08 +0900

    esbuild 자바스크립트 API를 이용한 빌드

---
+++ build.js
@@ -0,0 +1,35 @@
+import * as esbuild from 'esbuild'
+
+// html 
+esbuild
+    .build({
+        entryPoints: ['public/index.html'],
+        outfile: 'build/index.html',
+        loader: { '.html': 'copy' }
+    })
+    .then(() => console.log('⚡Bundle build complete ⚡'))
+    .catch(e => {
+        console.log('❌Failed to bundle ❌')
+        console.log(e)
+        process.exit(1)
+    })
+
+// js, css, files 
+esbuild
+    .build({
+        entryPoints: ['src/index.js'],
+        bundle: true, 
+        minify: true,
+        outfile: 'build/bundle.js',
+        loader: { '.js': 'jsx',  '.png': 'file', '.jpg': 'file', '.svg': 'file'},
+        format: 'cjs',
+        define: {
+            'process.env.REACT_APP_BASE_URL': '"https://vercel-express-deploy-tutorial.vercel.app"'
+        }
+    })
+    .then(() => console.log('⚡Bundle build complete ⚡'))
+    .catch(e => {
+        console.log('❌Failed to bundle ❌')
+        console.log(e)
+        process.exit(1)
+    })
---
+++ start.js
@@ -0,0 +1,39 @@
+import * as esbuild from 'esbuild'
+
+// html 
+esbuild
+    .build({
+        entryPoints: ['public/index.html'],
+        outfile: 'build/index.html',
+        loader: { '.html': 'copy' }
+    })
+    .then(() => console.log('⚡Bundle build complete ⚡'))
+    .catch(e => {
+        console.log('❌Failed to bundle ❌')
+        console.log(e)
+        process.exit(1)
+    })
+
+// js, css, files 
+esbuild
+    .context({
+        entryPoints: ['src/index.js'],
+        bundle: true, 
+        minify: true,
+        outfile: 'build/bundle.js',
+        loader: { '.js': 'jsx',  '.png': 'file', '.jpg': 'file', '.svg': 'file'},
+        format: 'cjs',
+        define: {
+            'process.env.REACT_APP_BASE_URL': '"https://vercel-express-deploy-tutorial.vercel.app"'
+        }
+    })
+    .then(async (ctx) => {
+        console.log('⚡Bundle build complete ⚡')
+        await ctx.watch().then(() => console.log('watching...'))
+        await ctx.serve({ servedir: 'build' }).then(() => console.log('serve at http://127.0.0.1:8000/'))
+    })
+    .catch(e => {
+        console.log('❌Failed to bundle ❌')
+        console.log(e)
+        process.exit(1)
+    })
